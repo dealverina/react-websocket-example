4a8837bebfaf0759d883c701cb54b643e7b2ab5e
Dea Alverina <dealverina@gmail.com>
2023-08-31 09:40:01 +0700

    add react-use-websocket 1. auto reconnect when state is closed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { useEffect, useState } from "react";
+
+const WebSocketDemo = () => {
+  const socketUrl = "wss://echo.websocket.events";
+  const [messageHistory, setMessageHistory] = useState([]);
+  const { lastMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('opened', new Date()),
+    shouldReconnect: (closeEvent) => true,
+    onReconnectStop: 1000,
+    onClose: () => console.log('closed', new Date()),
+    reconnectInterval: 1000,
+  });
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return (
+    <div>The WebSocket is currently {connectionStatus}</div>
+  )
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WebSocketDemo />
   );
 }
 
